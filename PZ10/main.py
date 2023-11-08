7dfb6a6dcdc74f4b56904df4939589b4f9a5c8c3
IlinaVN <139162448+IlinaVN@users.noreply.github.com>
2023-11-08 12:28:55 +0300

    Add files via upload

---
+++ PZ10/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+df = pd.read_excel("C:\PMiAOZD\PZ10\Машина 1.xlsx")
+df.insert(7, "Группа", 0)
+df.insert(8, "Категория", 0)
+df.insert(9, "Производитель", 0)
+krepezh_dict = {"Болт", "Гайка", "Штифт", "Шайба", "Шуруп"}
+decor_dict = {"Коврики", "Подушки"}
+for i in range(len(df)):
+    if i < 5:
+        df["Группа"].loc[df.index[i]] = "Сборка"
+    else:
+        df["Группа"].loc[df.index[i]] = "Компонент"
+    if df["Наименование компоненты"].loc[df.index[i]] in krepezh_dict:
+        df["Категория"].loc[df.index[i]] = "Крепёж"
+    elif df["Наименование компоненты"].loc[df.index[i]] in decor_dict:
+        df["Категория"].loc[df.index[i]] = "Декор"
+    elif df["Группа"].loc[df.index[i]] == "Сборка":
+        df["Категория"].loc[df.index[i]] = "Сборка"
+    else:
+        df["Категория"].loc[df.index[i]] = "Детали"
+    if "ГОСТ" in df['Код компоненты'].loc[df.index[i]]:
+        df["Производитель"].loc[df.index[i]] = "РФ по ГОСТ"
+    elif df["Код компоненты"].loc[df.index[i]].isascii():
+        df["Производитель"].loc[df.index[i]] = "Иностранные"
+    else:
+        df["Производитель"].loc[df.index[i]] = "РФ"
